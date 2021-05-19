c54802db09a800abbfa68ee5beef63af951952ca
Douglas Lima <douglaslima2363@gmail.com>
2021-05-18 21:34:31 -0300

    Iniciando Projeto Agenda-Api

---
+++ src/test/java/io/github/douglas2363/agenda/AgendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.douglas2363.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
